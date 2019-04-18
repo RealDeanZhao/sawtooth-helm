444aa999ec806734f6551c330509accb20ac8881
RealDeanZhao <zestxjest@outlook.com>
2019-04-18 18:31:59 +0800

    Fix some error and add more args

--- setup-network.sh
+++ setup-network.sh
@@ -1,68 +1,24 @@
-helm install \
---name sawtooth-0 \
---namespace sawtooth \
---set persistence.claimName=sawtooth-0-pvc \
---set persistence.pv.name=sawtooth-0-pv \
---set persistence.pv.path=/mnt/disks/sdb1/sawtooth-0-pv \
---set persistence.pvc.name=sawtooth-0-pvc \
---set persistence.storageClass.create=true \
---set persistence.storageClass.name=sawtooth-local-storage  \
---set persistence.pv.node=sawtooth-4 \
---set validator.genesis=true .
-
-helm install \
---name sawtooth-1 \
---namespace sawtooth \
---set persistence.claimName=sawtooth-1-pvc \
---set persistence.pv.name=sawtooth-1-pv \
---set persistence.pv.path=/mnt/disks/sdb1/sawtooth-1-pv \
---set persistence.pvc.name=sawtooth-1-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
---set persistence.pv.node=sawtooth-4 \
---set validator.args=tcp://sawtooth-0-validator:8800 .
-
-helm install \
---name sawtooth-2 \
---set persistence.claimName=sawtooth-2-pvc \
---set persistence.pv.name=sawtooth-2-pv \
---set persistence.pv.path=/mnt/disks/sdb1/sawtooth-2-pv \
---set persistence.pvc.name=sawtooth-2-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
---set persistence.pv.node=sawtooth-4 \
---set validator.args=tcp://sawtooth-0-validator:8800 .
-
-helm install \
---name sawtooth-3 \
---set persistence.claimName=sawtooth-3-pvc \
---set persistence.pv.name=sawtooth-3-pv \
---set persistence.pv.path=/mnt/disks/sdb1/sawtooth-3-pv \
---set persistence.pvc.name=sawtooth-3-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
---set persistence.pv.node=sawtooth-4 \
---set validator.args=tcp://sawtooth-0-validator:8800 .
-
-helm install \
---name sawtooth-4 \
---set persistence.claimName=sawtooth-4-pvc \
---set persistence.pv.name=sawtooth-4-pv \
---set persistence.pv.path=/mnt/disks/sdb1/sawtooth-4-pv \
---set persistence.pvc.name=sawtooth-4-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
---set persistence.pv.node=sawtooth-4 \
---set validator.args=tcp://sawtooth-0-validator:8800 .
-
-helm upgrade \
+helm upgrade -i \
 sawtooth-0 \
 --namespace sawtooth \
+--set validator.genesis=true \
 --set persistence.claimName=sawtooth-0-pvc \
 --set persistence.pv.name=sawtooth-0-pv \
 --set persistence.pv.path=/mnt/disks/sdb1/sawtooth-0-pv \
 --set persistence.pvc.name=sawtooth-0-pvc \
 --set persistence.storageClass.name=sawtooth-local-storage  \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--seeds tcp://sawtooth-0-validator:8800 --seeds tcp://sawtooth-1-validator:8800 --seeds tcp://sawtooth-2-validator:8800" .
+--set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 
+                     --scheduler parallel \
+                     --seeds tcp://sawtooth-2-validator:8008 \
+                     --seeds tcp://sawtooth-4-validator:8008 \
+                     --opentsdb-db metrics \
+                     --opentsdb-url http://sawtooth-stats-influxdb:8086" \
+--set rest_api.args="--opentsdb-db metrics \
+                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
+" .
 
-helm upgrade \
+helm upgrade -i \
 sawtooth-1 \
 --namespace sawtooth \
 --set persistence.claimName=sawtooth-1-pvc \
@@ -71,9 +27,17 @@ sawtooth-1 \
 --set persistence.pvc.name=sawtooth-1-pvc \
 --set persistence.storageClass.name=sawtooth-local-storage  \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-0-validator:8800" .
+--set validator.args="--peering=dynamic --seeds tcp://sawtooth-0-validator:8800 \
+                      --scheduler parallel \
+                      --seeds tcp://sawtooth-2-validator:8008 \
+                      --seeds tcp://sawtooth-3-validator:8008 \
+                      --opentsdb-db=metrics \
+                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
+--set rest_api.args="--opentsdb-db metrics \
+                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
+" .
 
-helm upgrade \
+helm upgrade -i \
 sawtooth-2 \
 --set persistence.claimName=sawtooth-2-pvc \
 --set persistence.pv.name=sawtooth-2-pv \
@@ -81,9 +45,17 @@ sawtooth-2 \
 --set persistence.pvc.name=sawtooth-2-pvc \
 --set persistence.storageClass.name=sawtooth-local-storage  \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-0-validator:8800" .
+--set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 \
+                      --scheduler parallel \
+                      --seeds tcp://sawtooth-0-validator:8008 \
+                      --seeds tcp://sawtooth-3-validator:8008 \
+                      --opentsdb-db=metrics \
+                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
+--set rest_api.args="--opentsdb-db metrics \
+                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
+" .
 
-helm upgrade \
+helm upgrade -i \
 sawtooth-3 \
 --set persistence.claimName=sawtooth-3-pvc \
 --set persistence.pv.name=sawtooth-3-pv \
@@ -91,9 +63,17 @@ sawtooth-3 \
 --set persistence.pvc.name=sawtooth-3-pvc \
 --set persistence.storageClass.name=sawtooth-local-storage  \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-0-validator:8800" .
+--set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 \
+                      --scheduler parallel \
+                      --seeds tcp://sawtooth-2-validator:8008 \
+                      --seeds tcp://sawtooth-0-validator:8008 \
+                      --opentsdb-db=metrics \
+                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
+--set rest_api.args="--opentsdb-db metrics \
+                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
+" .
 
-helm upgrade \
+helm upgrade -i \
 sawtooth-4 \
 --set persistence.claimName=sawtooth-4-pvc \
 --set persistence.pv.name=sawtooth-4-pv \
@@ -101,4 +81,12 @@ sawtooth-4 \
 --set persistence.pvc.name=sawtooth-4-pvc \
 --set persistence.storageClass.name=sawtooth-local-storage  \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-0-validator:8800" .
+--set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 \\
+                      --scheduler parallel \
+                      --seeds tcp://sawtooth-2-validator:8008 \
+                      --seeds tcp://sawtooth-3-validator:8008 \
+                      --opentsdb-db=metrics \
+                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
+--set rest_api.args="--opentsdb-db metrics \
+                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
+" .
