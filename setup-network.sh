e9f8a577e0c5c0bd859f2dce4c4870c9eabe6775
RealDeanZhao <zestxjest@outlook.com>
2019-04-23 15:39:21 +0800

    Change rest api config error

--- setup-network.sh
+++ setup-network.sh
@@ -1,41 +1,36 @@
 helm upgrade -i \
 sawtooth-0 \
---namespace sawtooth \
---set validator.genesis=true \
 --set persistence.claimName=sawtooth-0-pvc \
 --set persistence.pv.name=sawtooth-0-pv \
 --set persistence.pv.path=/mnt/disks/sdb1/sawtooth-0-pv \
 --set persistence.pvc.name=sawtooth-0-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
+--set persistence.storageClass.name=sawtooth-local-storage \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 
-                     --scheduler parallel \
-                     --seeds tcp://sawtooth-2-validator:8008 \
-                     --seeds tcp://sawtooth-4-validator:8008 \
-                     --opentsdb-db metrics \
-                     --opentsdb-url http://sawtooth-stats-influxdb:8086" \
---set rest_api.args="--opentsdb-db metrics \
-                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
-" .
+--set validator.genesis=true \
+--set validator.config.peering=dynamic \
+--set validator.config.scheduler=parallel \
+--set validator.config.seeds='[\"tcp\://sawtooth-1-validator:8800\"\, \"tcp\://sawtooth-2-validator:8800\"\, \"tcp\://sawtooth-3-validator:8800\"]' \
+--set opentsdb.url=http://sawtooth-stats-influxdb:8086 \
+--set opentsdb.db=metrics \
+--set opentsdb.username= \
+--set opentsdb.password= .
+
 
 helm upgrade -i \
 sawtooth-1 \
---namespace sawtooth \
 --set persistence.claimName=sawtooth-1-pvc \
 --set persistence.pv.name=sawtooth-1-pv \
 --set persistence.pv.path=/mnt/disks/sdb1/sawtooth-1-pv \
 --set persistence.pvc.name=sawtooth-1-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
+--set persistence.storageClass.name=sawtooth-local-storage \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-0-validator:8800 \
-                      --scheduler parallel \
-                      --seeds tcp://sawtooth-2-validator:8008 \
-                      --seeds tcp://sawtooth-3-validator:8008 \
-                      --opentsdb-db=metrics \
-                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
---set rest_api.args="--opentsdb-db metrics \
-                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
-" .
+--set validator.config.peering=dynamic \
+--set validator.config.scheduler=parallel \
+--set validator.config.seeds='[\"tcp\://sawtooth-0-validator:8800\"\, \"tcp\://sawtooth-2-validator:8800\"\, \"tcp\://sawtooth-3-validator:8800\"]' \
+--set opentsdb.url=http://sawtooth-stats-influxdb:8086 \
+--set opentsdb.db=metrics \
+--set opentsdb.username= \
+--set opentsdb.password= .
 
 helm upgrade -i \
 sawtooth-2 \
@@ -43,17 +38,15 @@ sawtooth-2 \
 --set persistence.pv.name=sawtooth-2-pv \
 --set persistence.pv.path=/mnt/disks/sdb1/sawtooth-2-pv \
 --set persistence.pvc.name=sawtooth-2-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
+--set persistence.storageClass.name=sawtooth-local-storage \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 \
-                      --scheduler parallel \
-                      --seeds tcp://sawtooth-0-validator:8008 \
-                      --seeds tcp://sawtooth-3-validator:8008 \
-                      --opentsdb-db=metrics \
-                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
---set rest_api.args="--opentsdb-db metrics \
-                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
-" .
+--set validator.config.peering=dynamic \
+--set validator.config.scheduler=parallel \
+--set validator.config.seeds='[\"tcp\://sawtooth-0-validator:8800\"\, \"tcp\://sawtooth-1-validator:8800\"\, \"tcp\://sawtooth-3-validator:8800\"]' \
+--set opentsdb.url=http://sawtooth-stats-influxdb:8086 \
+--set opentsdb.db=metrics \
+--set opentsdb.username= \
+--set opentsdb.password= .
 
 helm upgrade -i \
 sawtooth-3 \
@@ -61,36 +54,15 @@ sawtooth-3 \
 --set persistence.pv.name=sawtooth-3-pv \
 --set persistence.pv.path=/mnt/disks/sdb1/sawtooth-3-pv \
 --set persistence.pvc.name=sawtooth-3-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
+--set persistence.storageClass.name=sawtooth-local-storage \
 --set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 \
-                      --scheduler parallel \
-                      --seeds tcp://sawtooth-2-validator:8008 \
-                      --seeds tcp://sawtooth-0-validator:8008 \
-                      --opentsdb-db=metrics \
-                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
---set rest_api.args="--opentsdb-db metrics \
-                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
-" .
-
-helm upgrade -i \
-sawtooth-4 \
---set persistence.claimName=sawtooth-4-pvc \
---set persistence.pv.name=sawtooth-4-pv \
---set persistence.pv.path=/mnt/disks/sdb1/sawtooth-4-pv \
---set persistence.pvc.name=sawtooth-4-pvc \
---set persistence.storageClass.name=sawtooth-local-storage  \
---set persistence.pv.node=sawtooth-4 \
---set validator.args="--peering=dynamic --seeds tcp://sawtooth-1-validator:8800 \\
-                      --scheduler parallel \
-                      --seeds tcp://sawtooth-2-validator:8008 \
-                      --seeds tcp://sawtooth-3-validator:8008 \
-                      --opentsdb-db=metrics \
-                      --opentsdb-url=http://sawtooth-stats-influxdb:8086" \
---set rest_api.args="--opentsdb-db metrics \
-                     --opentsdb-url http://sawtooth-stats-influxdb:8086 \
-" .
-
+--set validator.config.peering=dynamic \
+--set validator.config.scheduler=parallel \
+--set validator.config.seeds='[\"tcp\://sawtooth-0-validator:8800\"\, \"tcp\://sawtooth-1-validator:8800\"\, \"tcp\://sawtooth-2-validator:8800\"]' \
+--set opentsdb.url=http://sawtooth-stats-influxdb:8086 \
+--set opentsdb.db=metrics \
+--set opentsdb.username= \
+--set opentsdb.password= .
 
 helm upgrade -i \
 sawtooth-4 \
@@ -102,7 +74,7 @@ sawtooth-4 \
 --set persistence.pv.node=sawtooth-4 \
 --set validator.config.peering=dynamic \
 --set validator.config.scheduler=parallel \
---set validator.config.seeds=[\"tcp\://sawtooth-0-validator:8008\"\, \"tcp\://sawtooth-1-validator:8008\"\, \"tcp\://sawtooth-2-validator:8008\"] \
+--set validator.config.seeds='[\"tcp\://sawtooth-0-validator:8800\"\, \"tcp\://sawtooth-1-validator:8800\"\, \"tcp\://sawtooth-2-validator:8800\"]' \
 --set opentsdb.url=http://sawtooth-stats-influxdb:8086 \
 --set opentsdb.db=metrics \
 --set opentsdb.username= \
